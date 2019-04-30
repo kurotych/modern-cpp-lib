0fbfe57aa54eb0f7cddc10d6b5a040db577d9325
Anatolii Kurotych <akurotych@gmail.com>
2019-04-30 22:50:13 +0300

    Add Gtest

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,13 @@
 #include <assert.h>
+#include <gtest/gtest.h>
 
 #include "../include/library.h"
 
-int main() {
-    assert(add(2, 2) == 4);
-    return 0;
+TEST(SimpleTest, NegativeNos) {
+    ASSERT_EQ(add(2, 2), 4);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
