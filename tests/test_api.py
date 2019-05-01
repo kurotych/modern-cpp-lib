49bc2dba9c7c6472558a66a793de8f504d54c567
Anatolii Kurotych <akurotych@gmail.com>
2019-05-01 19:39:54 +0300

    Add osx to travis

---
+++ tests/test_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import unittest
+import ctypes
+import platform
+
+class TestInterfaceFunctions(unittest.TestCase):
+
+        def setUp(self):
+                lib_path = "../build/libcprogen_core."
+                if platform.system() == 'Linux':
+                        lib_path += "so"
+                elif platform.system() == 'Darwin':
+                        lib_path += "dylib"
+                else:
+                        raise Exception('Not supported platform')
+
+                self.libcprogen_core = ctypes.cdll.LoadLibrary(lib_path)
+
+        def test_add(self):
+                self.assertEqual(self.libcprogen_core.add(2, 2), 4)
+
+if __name__ == '__main__':
+        unittest.main()
