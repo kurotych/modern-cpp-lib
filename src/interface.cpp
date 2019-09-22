60a7db339a09de0ea5ac0dc9b17339964d716994
Anatolii Kurotych <akurotych@gmail.com>
2019-09-22 12:30:39 +0300

    Add warnFoo for cppcheck example

--- src/interface.cpp
+++ src/interface.cpp
@@ -4,6 +4,12 @@ class API {
 public:
     int _add(int a, int b) { return a + b; }
     int _sub(int a, int b) { return a - b; }
+
+    // for cppcheck example
+    int _warnFoo(int idx = 6) {
+        int a[5] = {1,2,3,4,5};
+        return a[idx];
+    }
 };
 static API api;
 
@@ -14,4 +20,8 @@ int add(int a, int b) {
 
 int sub(int a, int b) {
     return api._sub(a, b);
+}
+
+int failFoo(int idx) {
+    return api._warnFoo(idx);
 }
